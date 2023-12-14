96f81a1b7b1ce6fe3dd3f577df7f5f3e4b0e658e
CZEJAY <calebjimmy67@gmail.com>
2023-12-14 19:06:56 +0100

    ui-3

--- app/(landing)/components/BentoGrid.tsx
+++ app/(landing)/components/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 const BentoGrid = () => {
@@ -7,22 +8,40 @@ const BentoGrid = () => {
 
         <div className="flex flex-col gap-2 bg-neutral-200/5 col-span-2 overflow-hidden w-fit rounded-sm border px-2 pb-2">
             <h4 className='text-sm font-semibold leading-tight capitalize text-center sm:text-xl text-blue-500'>Are you looking for the best teams?</h4>
-            <p className='text-sm sm:text-xl p-1 font-normal'> <span className='text-blue-300'>Cipherplus</span>&apos;s got you covered, with seasoned developers, with great team spirit your next big project will be delivered in no time.</p>
-            <Button className='bg-blue-500 hover:bg-blue-500/40 dark:text-white mt-auto '>View Teams</Button>
+            <p className='text-sm sm:text-lg p-1 font-normal'> <span className='text-blue-300'>Cipherplus</span> got you covered, with seasoned developers, with great team spirit your next big project will be delivered in no time.</p>
+            <Button className='bg-blue-500 hover:bg-blue-600 dark:text-white mt-auto '>View Teams</Button>
         </div>
 
         <div className="flex bg-neutral-200/5">
             <img src="/team.gif" className='rounded-sm w-full h-full object-cover' alt="" />
         </div>
 
-        <div className="flex p-4 bg-neutral-200"></div>
-        <div className="flex p-4 bg-neutral-200 col-span-2"></div>
-        <div className="flex p-4 bg-neutral-200 "></div>
-        <div className="flex p-4 flex-col gap-2 bg-neutral-200 col-span-2">
-          <h4 className='text-sm font-semibold leading-tight capitalize text-center sm:text-xl text-purple-500'>Do you need a standalone highly skilled Software Developer?</h4>
-          <p>We offer the best of our kind, so you'd minimize cost but yet get the job done with just one developer.</p>
-          <Button className='bg-purple-500 hover:bg-blue-500/40 dark:text-white '>View Developers</Button>
+        <div className="flex  bg-neutral-200/5 col-span-2">
+          
         </div>
+
+
+        <div className="flex p-4 bg-neutral-200/5">
+          
+        </div>
+
+
+        <div className="flex rounded-md border bg-neutral-200/5 ">
+        <Image src={"/coding.webp"} 
+          width={200}
+          height={200}
+          className='w-full h-full object-cover rounded-md'
+          alt='image'/>
+        </div>
+
+
+        <div className="flex p-4 flex-col gap-2 bg-neutral-200/5 col-span-2">
+          <h4 className='text-sm font-semibold leading-tight capitalize text-center sm:text-xl text-emerald-500'>Do you need a standalone highly skilled Software Developer?</h4>
+          <p className='text-sm sm:text-lg p-1 font-normal'>We offer the best of our kind, so you'd minimize cost but yet get the job done with just one developer.</p>
+          <Button className='bg-emerald-950 mt-auto hover:bg-emerald-800 dark:text-white '>View Developers</Button>
+        </div>
+
+        
     </div>
   )
 }
