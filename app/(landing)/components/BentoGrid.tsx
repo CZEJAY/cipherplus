b09336abec7b14ac5e2dd33661b1210b61c1d25c
CZEJAY <calebjimmy67@gmail.com>
2023-12-14 18:09:33 +0100

    ui

--- app/(landing)/components/BentoGrid.tsx
+++ app/(landing)/components/BentoGrid.tsx
@@ -1,12 +1,14 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const BentoGrid = () => {
   return (
-    <div className='grid grid-cols-3 gap-4 grid-flow-dense -bg-transparent backdrop-blur-xl w-[340px] px-4 sm:w-[700px] lg:w p-1 rounded-md '>
+    <div className='grid grid-cols-3 justify-center gap-4 grid-flow-dense bg-transparent backdrop-blur-xl w-[395px] px-4 sm:w-[700px] lg:w-[1000px] p-1 rounded-md '>
 
-        <div className="flex flex-col gap-2 bg-neutral-200/5 col-span-2 overflow-hidden w-fit rounded-sm border">
-            <h4 className='text-sm font-semibold leading-tight capitalize text-center'>Are you looking for the best teams?</h4>
-            <p className='text-sm p-1 font-'> <span className='text-blue-300'>Cipherplus</span>&apos;s got you covered, with seasoned developers, with great team spirit your next big project will be delivered in no time.</p>
+        <div className="flex flex-col gap-2 bg-neutral-200/5 col-span-2 overflow-hidden w-fit rounded-sm border px-2 pb-2">
+            <h4 className='text-sm font-semibold leading-tight capitalize text-center sm:text-xl text-blue-500'>Are you looking for the best teams?</h4>
+            <p className='text-sm sm:text-xl p-1 font-normal'> <span className='text-blue-300'>Cipherplus</span>&apos;s got you covered, with seasoned developers, with great team spirit your next big project will be delivered in no time.</p>
+            <Button className='bg-blue-500 hover:bg-blue-500/40 dark:text-white '>View Teams</Button>
         </div>
 
         <div className="flex bg-neutral-200/5">
@@ -14,8 +16,8 @@ const BentoGrid = () => {
         </div>
 
         <div className="flex p-4 bg-neutral-200"></div>
-        <div className="flex p-4 bg-neutral-200"></div>
-        <div className="flex p-4 bg-neutral-200 row-span-2"></div>
+        <div className="flex p-4 bg-neutral-200 col-span-2"></div>
+        <div className="flex p-4 bg-neutral-200 "></div>
         <div className="flex p-4 bg-neutral-200 col-span-2"></div>
     </div>
   )
