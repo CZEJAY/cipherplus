a4007620b3dbfe1de9478db0ded1c2fb95c3d038
CZEJAY <calebjimmy67@gmail.com>
2024-01-12 10:14:10 +0100

    prisma postinstall

--- actions/RemoteAPi.ts
+++ actions/RemoteAPi.ts
@@ -15,7 +15,7 @@ export const GetRemoteJob = async (company: string = "shopify") => {
       try {
           const response = await axios.request(options);
           console.log(response.data);
-          return response.data
+          return [response.data]
       } catch (error) {
           console.error(error);
           return []
